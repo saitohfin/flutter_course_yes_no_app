a0c620f2bfec03f16048861d237f44c22fd6a22e
Francisco Javier García Muñoz <javidesoft@gmail.com>
2023-08-16 18:10:13 +0200

    Init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes no app',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(onPressed: () {}, child: const Text("Click me")),
+        ),
+      ),
+    );
+  }
+}
