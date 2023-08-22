4160da010153bb507318e72b915d3733626a58de
Francisco Javier García Muñoz <javidesoft@gmail.com>
2023-08-22 17:17:37 +0200

    Added basic theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,24 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  static const List<Color> _colorThemes = [Colors.blue, Colors.green, Colors.orange];
+
+  final int _selectedColor;
+
+  AppTheme(this._selectedColor){
+    assert(_selectedColor >= 0 && _selectedColor < _colorThemes.length, 'Colors must be between 0 to ${_colorThemes.length}');
+  }
+
+
+
+  ThemeData theme(){
+    return ThemeData(
+        useMaterial3: true,
+      colorSchemeSeed: _colorThemes[_selectedColor],
+      brightness: Brightness.dark
+    );
+  }
+}
