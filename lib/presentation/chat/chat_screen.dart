49d62cea979160d2b83cd27a8ed4393a4ca44143
Francisco Javier García Muñoz <javidesoft@gmail.com>
2023-08-25 19:15:18 +0200

    ListView

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
 class ChatScreen extends StatelessWidget {
-
   const ChatScreen({super.key});
 
   @override
@@ -14,8 +12,32 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage("https://cdn131.picsart.com/350496690090201.jpg"),
+            backgroundImage:
+                NetworkImage("https://cdn131.picsart.com/350496690090201.jpg"),
           ),
+        ),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return Text('Indice: $index');
+                    })),
+            Text('Mundo')
+          ],
         ),
       ),
     );
