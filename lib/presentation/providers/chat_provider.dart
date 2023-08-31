a1a056c166cc21fc336139132c00c613864fc8b8
Francisco Javier García Muñoz <javidesoft@gmail.com>
2023-08-31 18:15:49 +0200

    Http call

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 
 import '../../domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScroll = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   final List<Message> messages = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
@@ -15,6 +17,18 @@ class ChatProvider extends ChangeNotifier {
 
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
+
+    if(text.endsWith('?')){
+      await herReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messages.add(herMessage);
 
     notifyListeners();
     moveScrollToBottom();
